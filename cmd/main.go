2046a646e39263839fd4d1c9d34ebce9eb072894
ArminEbrahimpour <armin890ebp@gmail.com>
2026-06-11 22:08:30 +0330

    Add: core app fucntions well

--- cmd/main.go
+++ cmd/main.go
@@ -3,25 +3,22 @@ package main
 import (
 	"Library/internal/server"
 	database "Library/pkg/db"
+	"Library/pkg/systray"
 	"context"
 	"log"
 	"net/http"
-	// "github.com/go-chi/chi/v5"
-	// "github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	// r := chi.NewRouter()
-	// r.Use(middleware.Logger)
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("welcome"))
-	// })
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	db, err := database.InitDB(ctx, "bookmarks.db")
+
 	if err != nil {
 		log.Fatal(err)
 	}
+	//log.Printf("Using database file at: %s", dbPath)
 	defer db.Close()
 
 	store := database.NewStore(db)
@@ -30,5 +27,13 @@ func main() {
 
 	addr := ":3333"
 	log.Printf("Serving on http://localhost%s", addr)
-	log.Fatal(http.ListenAndServe(addr, handler))
+	go func() {
+		log.Fatal(http.ListenAndServe(addr, handler))
+	}()
+
+	systray.Start()
+
+	// wait for quit signal from tray
+	<-systray.GetQuitChan()
+	log.Println("shutting down...")
 }
